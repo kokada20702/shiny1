81dc12783f44223c3901e137edef183021e7009b
Ken Okada <kokada20702@outlook.com>
2022-10-30 17:24:01 -0700

    dyn3

---
+++ dyn3.R
@@ -0,0 +1,45 @@
+library(tidyverse)
+library(shiny) 
+
+make_ui <- function(x, var) {
+  if ( is.numeric(x) ) {
+    rng <- range(x, na.rm = TRUE) 
+    sliderInput(var, var, min = rng[1], max = rng[2], value = rng)
+  }
+  else if ( is.factor(x) ) {
+    levs <- levels(x)
+    selectInput(var, var, choices = levs, selected = levs, multiple = TRUE)
+  }
+  else { NULL }
+}
+
+filter_var <- function(x, val) {
+  if (is.numeric(x)) {
+    !is.na(x) & x >= val[1] & x <= val[2] 
+  } else if (is.factor(x)) {
+    x %in% val 
+  } else {
+    TRUE
+  }
+}
+
+ui <- fluidPage(
+  sidebarLayout(
+    sidebarPanel(
+      map(names(iris), ~ make_ui(iris[[.x]], .x))
+    ), 
+    mainPanel(
+      tableOutput("data")
+    )
+  )
+)
+server <- function(input, output, session) {
+  selected <- reactive({
+    each_var <- map(names(iris), ~ filter_var(iris[[.x]], input[[.x]]))
+    reduce(each_var, ~ `&`(.x, .y) )
+  })
+  output$data <- renderTable(head(iris[selected(), ], 20))
+}
+
+shinyApp(ui, server) 
+
